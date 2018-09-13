661e0ab872bfc129ffe5032cf93d57f763dc75eb
Juan De la Cruz <juan.carlos.cc@outlook.com>
2018-09-13 18:28:04 -0500

    Verify that the user has the correct data

--- repositories/user-respository.js
+++ repositories/user-respository.js
@@ -2,4 +2,17 @@ module.exports = class UserRepository {
     constructor(db) {
         this.db = db
     }
+
+    /**
+     * Guarda el usuario en la base de datos devolviendo una promesa
+     * @param {User} user - Un objeto con una propiedad username y otra con propiedad password
+     */
+    save(user) {
+        return new Promise((resolve, reject) => {
+            if(user.username && user.password)
+                resolve("Okay")
+            else
+                reject({error: 0, message: "You need to send username and password"})
+        })
+    }
 }
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,5 +1,12 @@
 module.exports = function(app, userRepository) {
   app.post('/user', function(req, res) {
-      res.end()
+    userRepository
+      .save(req.body)
+      .then(item => res.end(item))
+      .catch(error => {
+        console.log(error)
+        res.statusCode = 400
+        res.send(error)
+      })
   })
 }
