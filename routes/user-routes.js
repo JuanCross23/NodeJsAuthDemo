8b753659536efbcac778b935cbee69f53ac736e1
Juan De la Cruz <juan.carlos.cc@outlook.com>
2018-09-13 18:31:41 -0500

    Add correct identation

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,12 +1,12 @@
 module.exports = function(app, userRepository) {
-  app.post('/user', function(req, res) {
-    userRepository
-      .save(req.body)
-      .then(item => res.end(item))
-      .catch(error => {
-        console.log(error)
-        res.statusCode = 400
-        res.send(error)
-      })
-  })
+    app.post('/user', function(req, res) {
+        userRepository
+            .save(req.body)
+            .then(item => res.end(item))
+            .catch(error => {
+                console.log(error)
+                res.statusCode = 400
+                res.send(error)
+            })
+    })
 }
