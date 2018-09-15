b7cbf5f910602c44c720981398196d3eff5012cb
Juan De la Cruz <juan.carlos.cc@outlook.com>
2018-09-14 20:11:23 -0500

    Send users to the client

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,4 +1,7 @@
 module.exports = function(app, userRepository) {
+    /**
+     * Método para crear usuarios
+     */
     app.post('/user', function(req, res) {
         userRepository
             .save(req.body)
@@ -9,6 +12,18 @@ module.exports = function(app, userRepository) {
                     res.statusCode = 400
                 else if(error.code == 1)
                     res.statusCode = 409
+                res.send(error)
+            })
+    })
+
+    app.get('/user', (req, res) => {
+        userRepository
+            .getAll()
+            .then(items => res.send(items))
+            .catch(error => {
+                console.log(error)
+                if(error.code == 0)
+                    res.statusCode = 500
                 res.send(error)
             })
     })
