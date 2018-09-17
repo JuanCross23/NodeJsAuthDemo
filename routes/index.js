e3c07d85dafd19b87eba18a9e60d49ca7cf409f9
Juan De la Cruz <juan.carlos.cc@outlook.com>
2018-09-16 23:17:01 -0500

    Inject the authorization service into users handler

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const userRoutes = require('./user-routes')
 
 module.exports = function(app, db) {
   const userRepository = require('../repositories/user-respository')
+  const authService = require('../services/authentication-service.js')
   authRoutes(app, db)
-  userRoutes(app, new userRepository(db))
+  userRoutes(app, new userRepository(db), new authService())
 }
