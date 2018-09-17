25194b1db99030b3028f63b01771716ea14a0811
Juan De la Cruz <juan.carlos.cc@outlook.com>
2018-09-17 08:26:47 -0500

    Validate has bearer and token in the authorization header

--- services/authentication-service.js
+++ services/authentication-service.js
@@ -1,11 +1,18 @@
 module.exports = class AuthenticationService {
     verifyAuthorization(request) {
         const authorizationHeader = request.get('Authorization')
+        console.log(authorizationHeader)
+        
         return new Promise(resolve => {
-            if(authorizationHeader)
+            if(authorizationHeader && hasValidFormat(authorizationHeader))
                 resolve(true)
             else 
                 resolve(false)
         })
     }
+}
+
+function hasValidFormat(authorizationHeader) {
+    const regex = new RegExp("Bearer [a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}")
+    return regex.test(authorizationHeader)
 }
