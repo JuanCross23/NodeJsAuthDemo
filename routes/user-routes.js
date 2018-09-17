9e1f82a0303cb77d69e6f4a70339c9b16413b267
Juan De la Cruz <juan.carlos.cc@outlook.com>
2018-09-16 23:25:39 -0500

    Add authorization to the put, get and delete endpoints

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,7 +1,7 @@
 var mongodb = require('mongodb');
 var ObjectId = mongodb.ObjectID;
 
-module.exports = function(app, userRepository) {
+module.exports = function(app, userRepository, authenticationService) {
     /**
      * Método para crear usuarios
      */
@@ -22,78 +22,97 @@ module.exports = function(app, userRepository) {
     /**
      * Método para obtener una lista de todos los usuarios
      */
-    app.get('/user', (req, res) => {
-        userRepository
-            .getAll()
-            .then(items => res.send(items))
-            .catch(error => {
-                console.log(error)
-                if(error.code == 0)
-                    res.statusCode = 500
-                res.send(error)
-            })
+    app.get('/user', async (req, res) => {
+        const isAuthenticated = await authenticationService.verifyAuthorization(req)
+        if(isAuthenticated) {
+            userRepository
+                .getAll()
+                .then(items => res.send(items))
+                .catch(error => {
+                    console.log(error)
+                    if(error.code == 0)
+                        res.statusCode = 500
+                    res.send(error)
+                })
+        } else {
+            res.statusCode = 401
+            res.end()
+        }
     })
 
     /**
      * Método para editar un usuario
      */
-    app.put('/user', (req, res) => {
-        const user = req.body;
-        
-        if(!hasCorrectProperties(user)) {
-            res.statusCode = 400
-            res.send({ code: 0, message: "Invalid model" })
-            return
+    app.put('/user', async (req, res) => {
+        const isAuthenticated = await authenticationService.verifyAuthorization(req)
+        if(isAuthenticated) {
+
+            const user = req.body;
+            
+            if(!hasCorrectProperties(user)) {
+                res.statusCode = 400
+                res.send({ code: 0, message: "Invalid model" })
+                return
+            }
+    
+            if(!idIsValid(user._id)) {
+                res.statusCode = 400
+                res.send({ code: 0, message: "Please provide a valid ID" })
+                return
+            }
+    
+            user._id = new ObjectId(user._id)
+    
+            userRepository
+                .update(req.body)
+                .then(() => {
+                    res.statusCode = 204
+                    res.end()
+                })
+                .catch(error => {
+                    console.log(error)
+                    if(error.code == 0)
+                        res.statusCode = 409
+                    else if(error.code == 1)
+                        res.statusCode = 404
+                    res.send(error)
+                })
+        } else {
+            res.statusCode = 401
+            res.end()
         }
-
-        if(!idIsValid(user._id)) {
-            res.statusCode = 400
-            res.send({ code: 0, message: "Please provide a valid ID" })
-            return
-        }
-
-        user._id = new ObjectId(user._id)
-
-        userRepository
-            .update(req.body)
-            .then(() => {
-                res.statusCode = 204
-                res.end()
-            })
-            .catch(error => {
-                console.log(error)
-                if(error.code == 0)
-                    res.statusCode = 409
-                else if(error.code == 1)
-                    res.statusCode = 404
-                res.send(error)
-            })
     })
     
     /**
      * Método para eliminar usuarios
      */
-    app.delete("/user/:ID",(req, res) => {
-        const userId = req.params.ID; 
-        if(!idIsValid(req.params.ID)) {
-            res.statusCode = 400;
-            res.send({code:0, message: "Please send a valid ID"})
-            return
+    app.delete("/user/:ID", async (req, res) => {
+        const isAuthenticated = await authenticationService.verifyAuthorization(req)
+        if(isAuthenticated){
+            const userId = req.params.ID; 
+            if(!idIsValid(req.params.ID)) {
+                res.statusCode = 400;
+                res.send({code:0, message: "Please send a valid ID"})
+                return
+            }
+    
+            userRepository.delete(new ObjectId(userId))
+                .then(() =>{
+                    res.statusCode = 204
+                    res.end()
+                })
+                .catch(error => {
+                    console.log(error)
+                    if(error.code == 2)
+                        res.statusCode = 404
+                    else
+                        res.statusCode = 500
+                    res.send(error)
+                })
+        } else {
+            res.statusCode = 401
+            res.end()
         }
-
-        userRepository.delete(new ObjectId(userId))
-            .then(() =>{
-                res.statusCode = 204
-                res.end()
-            })
-            .catch(error => {
-                console.log(error)
-                if(error.code == 2)
-                    res.statusCode = 404
-                else
-                    res.statusCode = 500
-                res.send(error)
-            })
     })
 }
 
