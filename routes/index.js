5818de7d419ae252607775c24af060de7a1c84ac
Juan De la Cruz <juan.carlos.cc@outlook.com>
2018-09-17 14:22:15 -0500

    Inject the db instance into the authorization service

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,5 @@ module.exports = function(app, db) {
   const userRepository = require('../repositories/user-respository')
   const authService = require('../services/authentication-service.js')
   authRoutes(app, db)
-  userRoutes(app, new userRepository(db), new authService())
+  userRoutes(app, new userRepository(db), new authService(db))
 }
